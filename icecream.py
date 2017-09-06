76a8d7d1922c307be02a1a1ee631e48a304fb872
merilindholm <31695165+merilindholm@users.noreply.github.com>
2017-09-06 16:55:32 +0300

    Add files via upload

---
+++ icecream.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+name='Meri Lindholm'
+
+iceCreamRating=10
+
+print('My name is', name, 'and I give eating ice cream a rating of', iceCreamRating, 'out of 10')
